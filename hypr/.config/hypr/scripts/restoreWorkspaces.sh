b3f9c6f2df9684f722b0de79131b477ed975f30a
IncogCyberpunk <incogcyberpunk@gmail.com>
2025-03-11 21:36:14 +0545

    Modified dotfiles after Installation of Arch

--- hypr/.config/hypr/scripts/restoreWorkspaces.sh
+++ hypr/.config/hypr/scripts/restoreWorkspaces.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Switch to workspace 3 and open VS Code in ~/.config/hypr/
+# Switch to workspace 3 and open ~/.config/hypr/ in NVIM
 hyprctl dispatch workspace 3
-code ~/.config/hypr/ & disown
-sleep 2
+kitty -e nvim ~/.config/hypr/  &
+sleep 1
 
-# Switch to workspace 4 and open VS Code in ~/.config/waybar/
+# Switch to workspace 4 and open ~/.config/waybar/ in NVIM
 hyprctl dispatch workspace 4
-code ~/.config/waybar/ & disown
-sleep 2
+kitty -e nvim ~/.config/waybar/  &
+sleep 1
 
 # Return to workspace 1 after doing the above
 hyprctl dispatch workspace 1
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -90,9 +90,6 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Disable touchpad completely inside neovim
-vim.api.nvim_cmd('set mouse=', opts)
-
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
@@ -105,10 +102,10 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = ''
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
@@ -283,7 +280,7 @@ require('lazy').setup({
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
 
-  { -- Useful plugin to show you pending keybinds.
+  {                     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
@@ -329,7 +326,7 @@ require('lazy').setup({
 
       -- Document existing key chains
       spec = {
-        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>c', group = '[C]ode',     mode = { 'n', 'x' } },
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
@@ -369,7 +366,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -477,7 +474,7 @@ require('lazy').setup({
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim',       opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
@@ -906,7 +903,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight'
     end,
   },
 
