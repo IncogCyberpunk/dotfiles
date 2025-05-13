dfc3cdb705a5a280701c4a29e78c40d70a015561
IncogCyberpunk <incogcyberpunk@gmail.com>
2025-05-13 09:22:26 +0545

    Customize sddm theme by using the sddm-theme and the setup.sh

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,10 @@
-require 'core.options' -- Load general options
-require 'core.keymaps' -- Load general keymaps
-require 'core.snippets' -- Custom code snippets
+require  'config.options' -- Load general options
+require  'config.keymaps' -- Load general keymaps
+require  'config.snippets' -- Custom code snippets
 
 -- Set up the Lazy plugin manager
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+-- Checks if the lazy.nvim file exists at the directory given by lazypath
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
@@ -11,25 +12,30 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     error('Error cloning lazy.nvim:\n' .. out)
   end
 end
-vim.opt.rtp:prepend(lazypath)
+-- Puts the lazy into runtimepath for neovim
+vim.opt.runtimepath:prepend(lazypath)
 
 -- Set up plugins
-require('lazy').setup {
-  require 'plugins.neotree',
-  require 'plugins.colortheme',
-  require 'plugins.bufferline',
-  require 'plugins.lualine',
-  require 'plugins.treesitter',
-  require 'plugins.telescope',
-  require 'plugins.lsp',
-  require 'plugins.autocompletion',
-  -- require 'plugins.none-ls',
-  require 'plugins.gitsigns',
-  require 'plugins.alpha',
-  require 'plugins.indent-blankline',
-  require 'plugins.misc',
-  require 'plugins.comment',
-}
+-- We are importing a lazy.lua file that's somewhere under the runtimepath for the neovim
+-- require ('lazy').setup {
+--   require  'plugins.neotree',
+--   require  'plugins.colortheme',
+--   require  'plugins.bufferline',
+--   require  'plugins.lualine',
+--   require  'plugins.treesitter',
+--   require  'plugins.telescope',
+--   require  'plugins.lsp',
+--   require  'plugins.autocompletion',
+--   -- require  'plugins.none-ls',
+--   require  'plugins.gitsigns',
+--   require  'plugins.alpha',
+--   require  'plugins.indent-blankline',
+--   require  'plugins.misc',
+--   require  'plugins.comment',
+-- }
+
+-- Better way to modularize 
+require('lazy').setup('plugins')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -45,8 +45,6 @@ return {
     -- [[ Configure Telescope ]]
     -- See `:help telescope` and `:help telescope.setup()`
     require('telescope').setup {
-      -- You can put your default mappings / updates / etc. in here
-      --  All the info you're looking for is in `:help telescope.setup()`
       defaults = {
         mappings = {
           i = {
@@ -83,6 +81,8 @@ return {
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+    vim.keymap.set('n', '<leader>rg', builtin.registers, { desc = 'List the registers' })
+    vim.keymap.set('n', '<leader>kt', builtin.colorscheme, { desc = 'Change Themes' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
@@ -91,6 +91,8 @@ return {
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      -- You can put your default mappings / updates / etc. in here
+      --  All the info you're looking for is in `:help telescope.setup()`
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
