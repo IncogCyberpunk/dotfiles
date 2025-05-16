998b6168b09e4fd85151d551c23893701604b35b
IncogCyberpunk <incogcyberpunk@gmail.com>
2025-05-16 22:42:41 +0545

    Fixed the ~/dotfiles/hypr/.config/hypr/.......... random directory that had appeared at ~/.config/hypr/

--- hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/~/dotfiles/hypr/.config/hypr/scripts/disableMic.sh
+++
@@ -1,4 +0,0 @@
-pactl set-source-mute @DEFAULT_SOURCE@ toggle 
-
-status=$(pactl get-source-mute @DEFAULT_SOURCE@ | grep -q 'yes' && echo ' <b>(MUTED)</b>' || echo ' <b>(UNMUTED)</b>')
-notify-send "Microphone Status" $status
