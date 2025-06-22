2e9f79a831e41a2d5f0362cc7fb5a5d5870a6f35
Incog <incogcyberpunk@gmail.com>
2025-06-22 20:09:33 +0545

    Removed unnecessary config files

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,9 +1,8 @@
 return {
   'nvimtools/none-ls.nvim',
-  enabled = false,
+  enabled = true,
   dependencies = {
-    'nvimtools/none-ls-extras.nvim',
-    'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
+    'nvim-lua/plenary.nvim',
   },
   config = function()
     local null_ls = require 'null-ls'
@@ -11,7 +10,7 @@ return {
     local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
     -- Formatters & linters for mason to install
-    require('mason-null-ls').setup {
+    require('mason-tool-installer').setup {
       ensure_installed = {
         'prettier', -- ts/js formatter
         'stylua', -- lua formatter
@@ -25,31 +24,11 @@ return {
 
     local sources = {
       diagnostics.checkmake,
-      formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
+      formatting.prettier,
       formatting.stylua,
       formatting.shfmt.with { args = { '-i', '4' } },
-      formatting.terraform_fmt,
-      require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
-      require 'none-ls.formatting.ruff_format',
     }
 
-    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-    null_ls.setup {
-      -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
-      sources = sources,
-      -- you can reuse a shared lspconfig on_attach callback here
-      on_attach = function(client, bufnr)
-        if client.supports_method 'textDocument/formatting' then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format { async = false }
-            end,
-          })
-        end
-      end,
-    }
+    vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format, {})
   end,
 }
