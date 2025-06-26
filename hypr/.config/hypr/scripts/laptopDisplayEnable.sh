fd4859106f100cf483f35472c70bc60f429a341b
Incog <incogcyberpunk@gmail.com>
2025-06-26 21:38:42 +0545

    Improvised the keybindings and the scripts to enable the displays

--- hypr/.config/hypr/scripts/laptopDisplayEnable.sh
+++ hypr/.config/hypr/scripts/laptopDisplayEnable.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e 'monitor = eDP-1,1920x1080,0x0,1\nmonitor = HDMI-A-1,disable' > ~/.config/hypr/monitors.conf
+echo -e 'monitor=eDP-1,1920x1080,0x0,1\nmonitor = HDMI-A-1,disable' > ~/.config/hypr/monitors.conf
 
 waypaper --restore
+
