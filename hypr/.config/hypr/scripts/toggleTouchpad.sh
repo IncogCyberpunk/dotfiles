44ded484e470ecc6f6780f49d1a6c620792c0ee4
Incog <incogcyberpunk@gmail.com>
2025-06-29 19:01:24 +0545

    Replaced dusnt with swaync

--- hypr/.config/hypr/scripts/toggleTouchpad.sh
+++ hypr/.config/hypr/scripts/toggleTouchpad.sh
@@ -3,10 +3,10 @@
 touchpadEnabledStatus=$(cat ~/.config/hypr/conf/touchpad/touchpad.conf | awk '/enabled/ {print $3}')
 printf "%s\n" "$touchpadEnabledStatus"
 if [[ "$touchpadEnabledStatus" == "true" ]]; then
-     dunstify "Touchpad" "Touchpad disabled"
+     notify-send "Touchpad" "Touchpad disabled"
      sed -i  's/enabled = true/enabled = false/' ~/.config/hypr/conf/touchpad/touchpad.conf
 else
-     dunstify "Touchpad" "Touchpad enabled"
+     notify-send "Touchpad" "Touchpad enabled"
     sed -i  's/enabled = false/enabled = true/' ~/.config/hypr/conf/touchpad/touchpad.conf
 fi
 
