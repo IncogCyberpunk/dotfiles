98a83bcb482473902214765620d4039a113b7092
Incog <incogcyberpunk@gmail.com>
2025-07-22 15:31:21 +0545

    Setup debuggers in nvim & added compose key options in hyprland

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -148,10 +148,6 @@ return {
         },
       }
 
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
       local capabilities = require('blink.cmp').get_lsp_capabilities()
 
       local servers = {
@@ -161,6 +157,7 @@ return {
         tailwindcss = {},
       jsonls = {},
       hyprls={},
+      cpptools={},
         lua_ls = {
 
           -- cmd = { ... },
