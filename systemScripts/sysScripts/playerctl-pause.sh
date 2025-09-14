28b32e5a3caa4fb52a97ff534ed74d351224b47e
Incog <incogcyberpunk@gmail.com>
2025-09-14 14:52:16 +0545

    Changes

--- systemScripts/sysScripts/playerctl-pause.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-bluetoothConnected=$(bluetoothctl info | awk '/Connected: yes/ {print "1"}')
-
-if [[ $(playerctl status) == "Playing" && bluetoothConnected -eq 1 ]]; then
-    playerctl pause
-fi
-# pactl set-sink-volume @DEFAULT_SINK@ 20%
