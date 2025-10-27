6358637ceb7b0e339314667b911730ced0c895fc
Incog <incogcyberpunk@gmail.com>
2025-10-27 20:47:48 +0545

    Added new lockscreen hypr script

--- hypr/.config/hypr/scripts/decreaseBrightness.sh
+++ hypr/.config/hypr/scripts/decreaseBrightness.sh
@@ -1,5 +1,4 @@
-# Specify the shell you want to use
-#!/bin/zsh  
+#!/usr/bin/env bash
 
 # Decreases the brightness of the laptop's screen by 10%. 
 # If the brightness is at 10%, it will only decrease to 5% 
