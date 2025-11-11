5d813ed8cde2710406001ab6f6358415067f55aa
Incog <incogcyberpunk@gmail.com>
2025-11-11 11:48:26 +0545

    Modified nvim-tree and telescope to display the .gitignore files by default

--- hypr/.config/hypr/scripts/lockscreen.sh
+++ hypr/.config/hypr/scripts/lockscreen.sh
@@ -2,4 +2,5 @@
 if pgrep -x hyprlock ; then
     pkill -x hyprlock
 fi
-hyprctl dispatch exec hyprlock && sleep 0.5 && hyprctl dispatch dpms off    
+
+sleep 0.5 && hyprctl dispatch dpms off && hyprlock
