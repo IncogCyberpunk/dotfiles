a775dca4fe1cf849ba0311e756e9437cc5ed0413
IncogCyberpunk <incogcyberpunk@gmail.com>
2025-12-24 10:31:34 +0545

    Modified the lsp.lua module path in nvim and modified some nvim autocommands

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,22 +1,14 @@
-return{
+return {
   -- Autoclose parentheses, brackets, quotes, etc.
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   config = function()
-    local npairs = require("nvim-autopairs")
-    local Rule = require("nvim-autopairs.rule")
+    local npairs = require 'nvim-autopairs'
+    local Rule = require 'nvim-autopairs.rule'
 
     -- First initialize autopairs
-    npairs.setup({
+    npairs.setup {
       disable_in_macro = false,
-    })
-
-    -- Then add your custom rule
-    npairs.add_rules({
-      Rule("if(", ")")
-        :use_key("(")                               -- tells autopairs to trigger on "("
-        :replace_endpair(function() return "  )" end) -- inserts "(  )"
-        :set_end_pair_length(2),                    -- place cursor 1 space before the end
-    })
+    }
   end,
 }
