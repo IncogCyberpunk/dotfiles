90cf4c10517e1f63ac5a202a4ea0c7130888e32f
IncogCyberpunk <incogcyberpunk@gmail.com>
2025-12-25 10:06:22 +0545

    Updated the autocommands and added support for custom snippest at $XDG_CONFIG_HOME/nvim/snippets

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -2,13 +2,5 @@ return {
   -- Autoclose parentheses, brackets, quotes, etc.
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  config = function()
-    local npairs = require 'nvim-autopairs'
-    local Rule = require 'nvim-autopairs.rule'
-
-    -- First initialize autopairs
-    npairs.setup {
-      disable_in_macro = false,
-    }
-  end,
+  opts = {},
 }
--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -3,44 +3,39 @@ return {
   enabled = true,
   dependencies = {
     {
-      "folke/lazydev.nvim",-- NOTE: This sets up lua-ls to be avaiable while editing nvim lua config files
-      ft="lua",
+      'folke/lazydev.nvim', -- NOTE: This sets up lua-ls to be avaiable while editing nvim lua config files
+      ft = 'lua',
       opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        library = {
+          { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
         },
       },
+    },
+    'rafamadriz/friendly-snippets',
   },
-  'rafamadriz/friendly-snippets' 
-},
-
-  -- use a release tag to download pre-built binaries
   version = '1.*',
   opts = {
-    keymap = { preset = 'default' },
-    appearance = {
-      nerd_font_variant = 'mono',
+    keymap = {
+      preset = 'default',
+      ['<C-l>'] = { 'snippet_forward', 'fallback' },
+      ['<C-j>'] = { 'snippet_backward', 'fallback' },
     },
-
+    appearance = { nerd_font_variant = 'mono' },
     completion = { documentation = { auto_show = true } },
-    signature = {enabled = true},
+    signature = { enabled = true },
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lazydev','lsp', 'path', 'snippets', },
+      default = { 'lazydev', 'lsp', 'path', 'snippets' },
       providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          score_offset = 100, -- priority
         },
+      },
     },
-
-    fuzzy = {implementation = "prefer_rust_with_warning"}
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
   },
   opts_extend = { 'sources.default' },
 }
