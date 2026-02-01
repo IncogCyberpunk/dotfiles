dcedcaa81ee85fd1bf0be43d9798dea3e0aa4e9a
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-02-01 20:34:12 +0545

    Update: Added calendar on hovering on waybar's clock module

--- hypr/.config/hypr/scripts/toggleAnimations.sh
+++ hypr/.config/hypr/scripts/toggleAnimations.sh
@@ -8,10 +8,18 @@ if [[ "$animationsState" -eq 1 ]]  ; then
     hyprctl keyword decorations:blur:enabled 0;\
     hyprctl keyword decorations:shadow:enabled 0 "
     notify-send "Animations & Decorations Disabled" -t 0
+
+    # Dim brightness to 20%
+    brightnessctl set 20% -s
 else
     hyprctl --batch "\
     keyword animations:enabled 1;\
     hyprctl keyword decorations:blur:enabled 1;\
     fyprctl keyword decorations:shadow:enabled 1 "
     notify-send "Animations & Decorations Enabled" -t 0
+    
+    # Restore brightness to previous level
+    brightnessctl -r
 fi
+
+
